8bf311c9abc002c85f317bc2185903eaeae8759d
redfox <stepperobot@gmail.com>
2022-05-06 00:08:51 +0300

    API_KEY set up as Query parameter

--- data/src/main/java/com/example/data/net/Service.kt
+++ data/src/main/java/com/example/data/net/Service.kt
@@ -2,13 +2,15 @@ package com.example.data.net
 
 import com.example.data.storage.model.cloudModel.MovieCloud
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Service {
 
     companion object {
         const val BASE_URL = "https://api.nytimes.com/"
+        const val API_KEY = "RfKDeuqwXGoiamMnWkIjouzeSB7KNAfA"
     }
 
-    @GET("svc/movies/v2/reviews/all.json?api-key=RfKDeuqwXGoiamMnWkIjouzeSB7KNAfA")
-    suspend fun fetchCloud(): MovieCloud
+    @GET("svc/movies/v2/reviews/all.json")
+    suspend fun fetchCloud(@Query("api-key") key: String = API_KEY): MovieCloud
 }
